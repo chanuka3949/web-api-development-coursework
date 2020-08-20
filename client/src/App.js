4252233eec6d9b2b99d4d2b57e619be9230f50cb
DESKTOP-VCOPUQ8\Hp User Lap <mahimasithuruwan@gmail.com>
2020-08-20 22:33:40 +0530

    initial structure - client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sample from './Components/Sample';
+
+
+function App() {
+  return (
+   <Sample></Sample>
+  );
+}
+
+export default App;
