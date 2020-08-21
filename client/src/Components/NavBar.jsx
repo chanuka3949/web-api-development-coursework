2acba7a1bec2009a7b2b9e35d6ba80e407aa6fde
darkslgithub <51149490+darkslgithub@users.noreply.github.com>
2020-08-21 22:22:13 +0530

    Designed the cart ui, navbar

---
+++ client/src/Components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <header className="header_header__3p7LH">
+          <a href="/">Store</a>
+          <a href="/about">About Us</a>
+          <a href="/about"> Contact Us</a>
+          <a href="/">Sign Up</a>
+          <a href="/">Sign In</a>
+          <a href="/cart">
+            <i className="fas fa-cart-plus"></i>
+            <svg fill="currentColor" viewBox="0 0 20 20">
+              <path d="" />
+            </svg>{" "}
+            Cart (1)
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./index.css";
+import CartParent from "./Components/CartParent";
+import NavBar from "./Components/NavBar";
+import "bootstrap/dist/css/bootstrap.css";
+import Cart from "./Components/Cart";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <NavBar />
+
+    <Route exact path="/cart" component={Cart} />
+    <Route exact path="/" component={CartParent} />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
